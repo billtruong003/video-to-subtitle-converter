0dea2237c608118b72de310d3d4a198fede1ab25
chautn6 <114427250+billtruong003@users.noreply.github.com>
2024-09-30 19:07:04 +0700

    * Update Deploy Port

--- deployver.py
+++ deployver.py
@@ -152,4 +152,5 @@ def download_file(filename):
     return send_from_directory(app.config['PROCESSED_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
+
